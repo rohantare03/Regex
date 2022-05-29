0c47e0980e396e78bcb5f76c2df58fabbae0fb60
rohantare03 <rohatare123@gmail.com>
2022-05-29 08:22:11 +0530

    [Rohan] Add : Validation of First Name

---
+++ RegexValidation/RegexValidation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RegexValidation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welocme to Regex");
+            UserRegistration userRegistration = new UserRegistration();
+            //uc1 : first  name
+            Console.WriteLine("Enter the First Name : ");
+            string First_Name = Console.ReadLine();
+            userRegistration.ValidateFirstName(First_Name);
+        }
+    }
+}
---
+++ RegexValidation/RegexValidation/UserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexValidation
+{
+    internal class UserRegistration
+    {
+        public string FirstName = ("^[A-Z]{1}[A-Za-z]{3,}$");
+        public void ValidateFirstName(string firstName)
+        {
+            Regex regex = new Regex(FirstName);
+            if (regex.IsMatch(firstName))
+            {
+                Console.WriteLine("First Name is Valid :" + firstName);
+            }
+            else
+            {
+                Console.WriteLine("First Name is Invalid");
+            }
+        }
+    }
+}
