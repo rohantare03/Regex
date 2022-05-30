41b0446e043f8db1d4a847dc3465a75111ebb3e5
rohantare03 <rohatare123@gmail.com>
2022-05-30 11:34:53 +0530

    [Rohan] Add : Validation of password having numeric values

--- RegexValidation/RegexValidation/Program.cs
+++ RegexValidation/RegexValidation/Program.cs
@@ -29,7 +29,7 @@
             string mobileNum = Console.ReadLine();
             userRegistration.Validate_Mobile(mobileNum);
 
-            //uc6 : password
+            //uc7 : password
             Console.WriteLine("----------------------------");
             Console.WriteLine("Enter the PassWord");
             string pass_Word = Console.ReadLine();
